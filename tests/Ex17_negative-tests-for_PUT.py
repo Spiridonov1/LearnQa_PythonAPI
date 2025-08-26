57540b7ef2fa958a61963f8ea6ae8eac8fc37315
Андрей Спиридонов <magnitslik@gmail.com>
2025-08-26 09:07:49 +0300

    commit Ex17

---
+++ tests/Ex17_negative-tests-for_PUT.py
@@ -0,0 +1,174 @@
+import requests
+import json
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserEdit(BaseCase):
+
+    URL="https://playground.learnqa.ru/api/user/"
+    LOGIN_URL = "https://playground.learnqa.ru/api/user/login"
+
+    def test_edit_just_created_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post(self.URL,data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        first_name = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = requests.post(self.LOGIN_URL, data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_name = "Changed Name"
+
+        response3 = requests.put(
+            f"{self.URL}{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+        response4 = requests.get(
+            f"{self.URL}{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            new_name,
+            "Wrong name of the user after edit"
+        )
+
+    def test_edit_user_unauthorized(self):
+        user_id = 2
+        new_name = "Unauthorized Edit"
+
+        response = requests.put(
+            f"{self.URL}{user_id}",
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response, 400)  # Или 403, в зависимости от API
+
+    def test_edit_user_by_another_user(self):
+        # REGISTER USER 1
+        register_data1 = self.prepare_registration_data()
+        response1 = requests.post(self.URL, data=register_data1)
+        Assertions.assert_code_status(response1, 200)
+        user_id1 = self.get_json_value(response1, "id")
+
+        # LOGIN USER 1
+        login_data1 = {
+            'email': register_data1['email'],
+            'password': register_data1['password']
+        }
+        response2 = requests.post(self.LOGIN_URL, data=login_data1)
+        auth_sid1 = self.get_cookie(response2, "auth_sid")
+        token1 = self.get_header(response2, "x-csrf-token")
+
+        # REGISTER USER 2
+        register_data2 = self.prepare_registration_data()
+        response3 = requests.post(self.URL, data=register_data2)
+
+        Assertions.assert_code_status(response3, 200)
+        user_id2 = self.get_json_value(response3, "id")
+
+        # EDIT USER 2 BY USER 1
+        new_name = "Hacked Name"
+        response4 = requests.put(
+            f"{self.URL}{user_id2}",
+            headers={"x-csrf-token": token1},
+            cookies={"auth_sid": auth_sid1},
+            data={"firstName": new_name}
+        )
+        Assertions.assert_code_status(response4, 400)
+
+    def test_edit_user_email_invalid_format(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post(self.URL, data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': register_data['email'],
+            'password': register_data['password']
+        }
+
+        response2 = requests.post(self.LOGIN_URL, data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        invalid_email = "invalid_email"
+
+        response3 = requests.put(
+            f"{self.URL}{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": invalid_email}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_has_key(response3, "error")  # Проверка наличия ключа "error"
+
+        response_data = json.loads(response3.text) # Преобразуем текст ответа в JSON
+        assert "Invalid email format" in response_data["error"] # Уточняем, что в сообщении об ошибке есть  "Invalid email format"
+
+
+    def test_edit_user_short_first_name(self):
+
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = requests.post(self.URL, data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': register_data['email'],
+            'password': register_data['password']
+        }
+
+        response2 = requests.post(self.LOGIN_URL, data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        short_name = "A"
+
+        response3 = requests.put(
+            f"{self.URL}{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": short_name}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+        Assertions.assert_json_has_key(response3, "error")
+
+        response_data = json.loads(response3.text)
+        assert "The value for field `firstName` is too short" in response_data["error"] # Уточняем, что в сообщении об ошибке есть  "firstName cannot be shorter than 2"
+
